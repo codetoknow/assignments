8ffd09e4797c7d94cc619f7bc1ef7d60269b992f
Anirudh Singh Shekhawat <anirudhsingh.shekhawat@external.stellantis.com>
2025-02-07 12:39:22 +0530

    Display Text Assignment

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import { useState } from 'react'; // import useState react hook
+import DisplayTextAssignment from "./DisplayTextAssignment";
 
 const App = () => {
-  const [text, setText] = useState(""); // Initializing a state with empty string
-
-  const onShowText = () => {
-    setText("Hello, world!"); // Setting the state
-  }
-
   return (
-    <div>
-      {/* Displaying the state that was set in state handler (above) */}
-      <p>Click the button to view text here: <strong>{text}</strong></p> 
-
-      {/* Clicking on button will call the handle function onShowText */}
-      <button onClick={onShowText}>Click Me!</button>
-    </div>
+    <DisplayTextAssignment />
   );
 }
 
---
+++ src/DisplayTextAssignment.js
@@ -0,0 +1,21 @@
+import { useState } from 'react'; // import useState react hook
+
+const DisplayTextAssignment = () => {
+  const [text, setText] = useState(""); // Initializing a state with empty string
+
+  const onShowText = () => {
+    setText("Hello, world!"); // Setting the state
+  }
+
+  return (
+    <div>
+      {/* Displaying the state that was set in state handler (above) */}
+      <p>Click the button to view text here: <strong>{text}</strong></p> 
+
+      {/* Clicking on button will call the handle function onShowText */}
+      <button onClick={onShowText}>Click Me!</button>
+    </div>
+  );
+}
+
+export default DisplayTextAssignment;
